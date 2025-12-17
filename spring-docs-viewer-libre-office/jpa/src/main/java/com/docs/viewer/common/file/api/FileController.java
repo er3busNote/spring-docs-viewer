bda021ad9cefa11285931b22eaa9c5fcc965252a
er3busnote <qudwn0768@naver.com>
2025-12-17 20:24:38 +0900

    feat: libre module api → jpa file controller

---
+++ spring-docs-viewer-libre-office/jpa/src/main/java/com/docs/viewer/common/file/api/FileController.java
@@ -0,0 +1,11 @@
+package com.docs.viewer.common.file.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/file")
+public class FileController {
+}
