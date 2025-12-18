fd08d7948959dc01d97844b614c8261956092a1e
er3busnote <qudwn0768@naver.com>
2025-12-18 21:07:07 +0900

    feat: libre module api → jpa file utils

---
+++ spring-docs-viewer-libre-office/jpa/src/main/java/com/docs/viewer/common/file/entity/File.java
@@ -0,0 +1,76 @@
+package com.docs.viewer.common.file.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "CMM_FILE")   // 공통 파일
+@Getter
+@Setter
+@EqualsAndHashCode(of = "fileCode")
+@Builder @NoArgsConstructor
+@AllArgsConstructor
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "fileCode")
+    @SequenceGenerator(
+            name = "fileCode",
+            sequenceName = "CMM_FILE_SEQUENCE",
+            initialValue = 100000,
+            allocationSize = 1
+    )
+    @Column(name = "FILE_CD", columnDefinition = "BIGINT(20) COMMENT '파일코드'")
+    private Long fileCode;
+
+    @Column(name = "FILE_NM", columnDefinition = "VARCHAR(100) COMMENT '파일명'")
+    private String fileName;
+
+    @Column(name = "FILE_PATH", columnDefinition = "VARCHAR(200) COMMENT '파일경로'")
+    private String filePath;
+
+    @Column(name = "FILE_TYPE", columnDefinition = "VARCHAR(100) COMMENT '파일유형'")
+    private String fileType;
+
+    @Column(name = "FILE_SIZE", columnDefinition = "INT(11) COMMENT '파일크기'")
+    private Integer fileSize;
+
+    @Builder.Default
+    @Column(name = "CLOUD_YN", nullable = false, columnDefinition = "CHAR(1) COMMENT '버킷사용여부'")
+    private Character cloudYn = 'N';
+
+    @Column(name = "REGR", nullable = false, columnDefinition = "VARCHAR(30) COMMENT '등록자'")
+    @NotBlank
+    private String register;
+
+    @Builder.Default
+    @Column(name = "REG_DT", updatable = false, nullable = false, columnDefinition = "TIMESTAMP DEFAULT current_timestamp() COMMENT '등록일시'")
+    @CreationTimestamp  // INSERT 시 자동으로 값을 채워줌
+    private LocalDateTime registerDate = LocalDateTime.now();
+
+    @Column(name = "UPDR", nullable = false, columnDefinition = "VARCHAR(30) COMMENT '수정자'")
+    @NotBlank
+    private String updater;
+
+    @Builder.Default
+    @Column(name = "UPD_DT", nullable = false, columnDefinition = "TIMESTAMP DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '수정일시'")
+    @UpdateTimestamp    // UPDATE 시 자동으로 값을 채워줌
+    private LocalDateTime updateDate = LocalDateTime.now();
+
+    public static File of(String fileName, String filePath, String fileType, Integer fileSize, Character cloudYn) {
+        return File.builder()
+                .fileName(fileName)
+                .filePath(filePath)
+                .fileType(fileType)
+                .fileSize(fileSize)
+                .cloudYn(cloudYn)
+                .register("M000000002")
+                .updater("M000000002")
+                .build();
+    }
+}
